dd5a929d3c2772a639b23de362bb3a30bdfd60ef
Lars V <87282@glr.nl>
2024-03-25 15:28:46 +0100

    cahnged the router in useeffect

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,16 @@
 "use client";
 
-import { useRouter } from "next/navigation";
+import { useEffect } from 'react';
+import { useRouter } from 'next/navigation';
+
 const Page = () => {
   const router = useRouter();
 
-  return (
-      router.push('/register')
-  );
+  useEffect(() => {
+    router.push('/register');
+  }, [router]);
+
+  return null;
 };
 
 export default Page;
